dec9be45f4082669bd3b5f03d2d5de2c89befd49
alexcercos <44820718+alexcercos@users.noreply.github.com>
2025-06-21 13:01:31 +0200

    Remove exception

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -99,7 +99,8 @@ class ApiService {
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
-      throw Exception('Failed to load sessions');
+      return [];
+      // throw Exception('Failed to load sessions');
     }
   }
 
